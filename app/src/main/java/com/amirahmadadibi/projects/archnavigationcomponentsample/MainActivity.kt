1a1efa2f30cc2178c6a9554b0d7e05aecd4c2bf3
AmirahmadAdibi <amirahmadadibi@gmail.com>
2020-01-01 10:46:46 +0330

    initialization commit

---
+++ app/src/main/java/com/amirahmadadibi/projects/archnavigationcomponentsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.amirahmadadibi.projects.archnavigationcomponentsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
